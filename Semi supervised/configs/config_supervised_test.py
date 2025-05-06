1926a7dc7cf282817cb4c35065c1d16d1f467ab1
c11na <incohatus@outlook.com>
2025-05-07 05:29:44 +0800

    test

--- Semi supervised/configs/config_supervised_test.py
+++ Semi supervised/configs/config_supervised_test.py
@@ -25,7 +25,7 @@ parser.add_argument('--max_iterations', type=int,
                     default=30000, help='maximum epoch number to train')
 parser.add_argument('--epoch_num', type=int, default=50,
                     help='epochs')
-parser.add_argument('--batch_size', type=int, default=4,
+parser.add_argument('--batch_size', type=int, default=2,
                     help='batch_size per gpu')
 parser.add_argument('--deterministic', type=int,  default=1,
                     help='whether use deterministic training')
